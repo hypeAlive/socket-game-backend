b0c33ac0a81a969c56dea394f7a3663fee4368ce
Alive <nicolasfr002@gmail.com>
2024-07-03 23:30:35 +0200

    [types][game] added game types

---
+++ server/src/types/game/game.type.ts
@@ -0,0 +1,33 @@
+import BaseGame from "../../base/game/BaseGame.js";
+
+/**
+ * Typdefinition für ein Spiel.
+ * Wird benutzt, um ein Spiel zu registrieren.
+ *
+ * @namespace - eindeutiger Namespace des Spiels
+ * @gameClass - Funktion, die eine Instanz des Spiels erstellt
+ */
+export interface GameType<PD extends PlayerData, GD extends GameData> {
+    namespace: string;
+    gameClass: () => BaseGame<PD, GD>;
+}
+
+export type PlayerData = {
+    playerId: string,
+
+}
+
+export type GameData = {
+    gameId: GameId,
+    playerIds: string[],
+    currentPlayerId: string,
+    winnerId: string | null,
+}
+
+export enum GameState {
+    WAITING = 'WAITING',
+    STARTED = 'STARTED',
+    ENDED = 'ENDED'
+}
+
+export type GameId = [gameNamespace: string, identifier: number];
