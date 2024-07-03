53b6dc91087fb18140c28bfb11ef8ac34ee5a53f
Alive <nicolasfr002@gmail.com>
2024-07-04 01:37:41 +0200

    [project][] updated scripts

--- setup.bash
+++ setup.bash
@@ -1,9 +1,13 @@
-#!bin bash
+#!/bin/bash
 
 cd ./types
 npm install
 npm run build
 
+echo "Types setup complete"
+
 cd ../server
 npm install
 npm run build
+
+echo "Server setup complete"
--- start.bash
+++ start.bash
@@ -1,6 +1,14 @@
-#!bin bash
+#!/bin/bash
 
+
+./setup.bash
+echo "Setup complete"
 # This script is used to start the application
 
 cd ./server
 npm start
+
+echo "Server started"
+
+# Halten Sie das Terminal offen
+read -p "Press enter to exit"
